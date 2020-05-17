4a5663140f84f0ef4cbe0447b56a8dd356dd15d2
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-05-17 14:38:02 +1200

    Updates

---
+++ precache-manifest.48f1d716ebfeb189dca52de15fe72ad0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90a05fc13055f678a0d411ad8660a8d",
+    "url": "/eugra/index.html"
+  },
+  {
+    "revision": "053398a4a1d3801330fd",
+    "url": "/eugra/static/css/main.5d531811.chunk.css"
+  },
+  {
+    "revision": "7ab01b80bc6b4cfedf10",
+    "url": "/eugra/static/js/2.6c555d48.chunk.js"
+  },
+  {
+    "revision": "d785ce58cb5d444e8ab02aec6a8b59c9",
+    "url": "/eugra/static/js/2.6c555d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053398a4a1d3801330fd",
+    "url": "/eugra/static/js/main.08c8a030.chunk.js"
+  },
+  {
+    "revision": "997935bff15afa42a231",
+    "url": "/eugra/static/js/runtime-main.d3a1fe57.js"
+  },
+  {
+    "revision": "5addf8787aa2ff6ee44dd9de0a35c203",
+    "url": "/eugra/static/media/Untitled-2.5addf878.png"
+  },
+  {
+    "revision": "77436d7252a92fc087286076445492e3",
+    "url": "/eugra/static/media/google-play-badge-large.77436d72.png"
+  },
+  {
+    "revision": "8cedafee1f419059451ff1cf0b400427",
+    "url": "/eugra/static/media/google-play-badge.8cedafee.png"
+  },
+  {
+    "revision": "814ad0c994e9d826b683c3bcd69a84ce",
+    "url": "/eugra/static/media/menu.814ad0c9.png"
+  },
+  {
+    "revision": "26ab6ef184b0d2b6cef8a1e288139a77",
+    "url": "/eugra/static/media/plus.26ab6ef1.png"
+  }
+]);
